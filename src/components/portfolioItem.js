b7836c2a8c9fd104877903e29ad5cba099f86053
Ferdi Eraslan <frdiersln@hotmail.com>
2025-03-02 05:41:42 +0300

    handled video playback on hover; update styles for video section

--- src/components/portfolioItem.js
+++ src/components/portfolioItem.js
@@ -1,15 +1,40 @@
-import React from 'react';
+"use client"
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Link2 } from 'lucide-react';
 
 import "../app/styles/portfolioItem.css";
 
 const PortfolioItem = ({ title, description, link, videoPath }) => {
+    const videoRef = useRef(null);
+
+    const handleMouseEnter = () => {
+        if (videoRef.current) {
+            videoRef.current.play();
+        }
+    };
+
+    const handleMouseLeave = () => {
+        if (videoRef.current) {
+            videoRef.current.pause();
+            videoRef.current.currentTime = 0;
+        }
+    };
+
     return (
         <div className="portfolio-item">
             <h2 className='title'>{title}</h2>
-            <div className="video-section">
-                <video src={videoPath} />
+            <div className="video-section"
+                 onMouseEnter={handleMouseEnter}
+                 onMouseLeave={handleMouseLeave}
+            >
+                <video 
+                ref={videoRef}
+                src={videoPath}
+                muted  
+                loop   
+                playsInline // Better mobile support
+                />
                 <a href={link} target="_blank" rel="noopener noreferrer">
                     <Link2 size={19}/> {link} 
                 </a>
